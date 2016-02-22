dddec829f0731630c70591d38af839ce854061cb
Yaroslav Yermilov <yermilov17@gmail.com>
2016-02-22 15:43:21 +0200

    assignment 2 initial

--- conFusion/Gulpfile.js
+++ conFusion/Gulpfile.js
@@ -23,10 +23,12 @@ gulp.task('jshint', function() {
 
 gulp.task('usemin',['jshint'], function () {
   return gulp .src('app/**/*.html')
-              .pipe(usemin({
-                css:[minifycss(), rev()],
-                js: [ngannotate(), uglify(), rev()]
-              }))
+              .pipe(usemin(
+              //{
+              //  css:[minifycss(), rev()],
+              //  js: [ngannotate(), uglify(), rev()]
+              //}
+              ))
               .pipe(gulp.dest('dist/'));
 });
 
