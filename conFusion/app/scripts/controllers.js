22deb0867635da16db061d402cd69f94ba558a21
Yaroslav Yermilov <yermilov17@gmail.com>
2016-02-23 23:33:25 +0200

    week 3 task 1

---
+++ conFusion/app/scripts/controllers.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular .module('confusionApp')
+
+        .controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes= menuFactory.getDishes();
+
+             $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = { mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            $scope.invalidChannelSelection = false;
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+
+            $scope.sendFeedback = function() {
+                console.log($scope.feedback);
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")&& !$scope.feedback.mychannel) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        .controller('DishDetailController', ['$scope', 'menuFactory', function($scope, menuFactory) {
+            $scope.dish= menuFactory.getDish(3);
+        }])
+
+        .controller('DishCommentController', ['$scope', function($scope) {       
+            $scope.comment = { name:"", rating:"5", comment:"", date:"now" };
+            
+            $scope.submitComment = function () {
+                $scope.comment.date = new Date().toISOString();
+                $scope.dish.comments.push($scope.comment);
+                $scope.commentForm.$setPristine();
+                $scope.comment = { name:"", rating:"5", comment:"", date:"now" };
+            };
+        }]);
