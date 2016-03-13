87e89067b12fe4662cd8eebaa416163f9f3182d1
Yaroslav Yermilov <yermilov17@gmail.com>
2016-03-13 20:35:29 +0200

    week 4 task 4.2

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -46,7 +46,7 @@ angular .module('confusionApp')
         }])
 
         .controller('ContactController', ['$scope', function($scope) {
-            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            $scope.feedback = { mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
             
             var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
             
